7b77eac9b60f87519f0841a5523c990fa3302951
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2023-01-14 22:57:14 -0600

    Configure Tailwind, Keyrune, and Mana packages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,38 @@
-import Head from 'next/head';
 import Layout from './components/Layout';
+
+const sets: string[] = [
+  'znr',
+  'khm',
+  'stx',
+  'afr',
+  'mid',
+  'vow',
+  'neo',
+  'snc',
+  'dmu',
+  'bro',
+];
+
+const answer: string = 'znr';
 
 export default function Home() {
   return (
     <Layout>
-      Welcome <i className="ss ss-znr"></i> <i className="ms ms-p"></i>
+      Play as much Enchant Worldle as you like!
+      <div className="flex">
+        {sets.map((set, index) => {
+          return (
+            /* Need some way to highlight the sets individually in yellow or green */
+            <div key={`${set}`} className="px-1">
+              <i
+                className={`ss-2x ss ss-${set} ${
+                  set === answer ? 'text-green-500' : null
+                }`}
+              ></i>
+            </div>
+          );
+        })}
+      </div>
     </Layout>
   );
 }
